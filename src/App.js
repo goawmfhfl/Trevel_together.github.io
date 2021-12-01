14de35eaeead7ad9ed7152b011dd42e2452a9679
jae <goawmfhfl1@naver.com>
2021-12-01 16:13:38 +0900

    feat: 전체 레이아웃
    1. header, nav HTML, SCSS적용, scss->component폴더로 변경
    
    2. 폴더 아키텍처와 홈페이지 레이아웃을 다듬는 작업을 했다
    SCSS폴더를 component로 바꾼이유는 js와 scss 파일을 함께 사용하기 위함이다.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className="head_title">Travel together</h1>
+        <p className="head_icon">✈️</p>
       </header>
+      <nav>
+        <div className="iconbox">
+          <button>
+            <i className="fas fa-heart"></i>
+            <p>travel</p>
+          </button>
+          <button>
+            <i className="fas fa-heart"></i>
+            <p>travel</p>
+          </button>
+          <button>
+            <i className="fas fa-heart"></i>
+            <p>travel</p>
+          </button>
+          <button>
+            <i className="fas fa-heart"></i>
+            <p>travel</p>
+          </button>
+        </div>
+      </nav>
     </div>
   );
 }
