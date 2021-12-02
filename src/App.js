ded5ff03c22e1dd2fa36f53948d2703eae18ba79
jae <goawmfhfl1@naver.com>
2021-12-02 20:17:09 +0900

    feat: card & image
    1. 카테고리버튼, 이미지 카드 업로드
    
    2. 카테고리 버튼을 만들어서 버튼을 클릭 할 경우 해당 카테고리에 관련한 정보를 나타날 수 있게 할 예정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-
-import './App.scss'
+import React from 'react'
+import img from './test.jpg'
+import './App.scss';
+// import travelNav from './travelNav.js'
 
 function App() {
   return (
@@ -8,34 +10,160 @@ function App() {
         <h1 className="head_title">Travel together</h1>
         <p className="head_icon">✈️</p>
       </header>
-      <nav className="nav-travel">
-        <ul className="list-travelNav">
-          <li className="link-travelNav on">
-            <a href="#none" className="link-box">
-              <i className="link-icon">🏖</i>
-              <span className="link-tit">travel</span>
+      <div className="container">
+        <nav className="nav-travel">
+          <ul className="list-travelNav">
+            <li className="link-travelNav on">
+              <a href="#none" className="link-box">
+                <i className="link-icon">🏖</i>
+                <span className="link-tit">travel</span>
+              </a>
+            </li>
+            <li className="link-travelNav">
+              <a href="#none" className="link-box">
+                <i className="link-icon">🏖</i>
+                <span className="link-tit">travel</span>
+              </a>
+            </li>
+            <li className="link-travelNav">
+              <a href="#none" className="link-box">
+                <i className="link-icon">🏖</i>
+                <span className="link-tit">travel</span>
+              </a>
+            </li>
+            <li className="link-travelNav">
+              <a href="#none" className="link-box">
+                <i className="link-icon">🏖</i>
+                <span className="link-tit">travel</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <article className="category-btn">
+          <button type="button" className="category-btn">Category</button>
+          <ul className="category-list">
+            <li><a href="#none">infj</a></li>
+            <li><a href="#none">infj</a></li>
+            <li><a href="#none">infj</a></li>
+          </ul>
+        </article>
+        <ul className="card-list">
+          <li>
+            <a href="#none">
+              <div className="card-cover">
+                <img src={img} alt="이미지" className="card-img" />
+              </div>
+              <div className="card-cont">
+                <div className="card-cate">
+                  <span className="card-cate-tit">Lorem, ipsum.</span>
+                  <span className="card-cate-star">⭐️⭐️⭐️⭐️⭐️</span>
+                </div>
+                <div className="card-desc">
+                  <div class="card-desc-tit multi-ellipsis"> Lorem ipsum dolor sit amet Lorem ipsum dolor
+                  sit amet consectetur adipisicing elit. Quibusdam, voluptatem?</div>
+                  <span class="card-desc-price">USD 9.45</span>
+                  <span class="card-desc-sale">10.22</span>
+                </div>
+              </div>
             </a>
           </li>
-          <li className="link-travelNav">
-            <a href="#none" className="link-box">
-              <i className="link-icon">🏖</i>
-              <span className="link-tit">travel</span>
+          <li>
+            <a href="#none">
+              <div className="card-cover">
+                <img src={img} alt="이미지" className="card-img" />
+              </div>
+              <div className="card-cont">
+                <div className="card-cate">
+                  <span className="card-cate-tit">Lorem, ipsum.</span>
+                  <span className="card-cate-star">⭐️⭐️⭐️⭐️⭐️</span>
+                </div>
+                <div className="card-desc">
+                  <div class="card-desc-tit multi-ellipsis"> Lorem ipsum dolor sit amet Lorem ipsum dolor
+                  sit amet consectetur adipisicing elit. Quibusdam, voluptatem?</div>
+                  <span class="card-desc-price">USD 9.45</span>
+                  <span class="card-desc-sale">10.22</span>
+                </div>
+              </div>
             </a>
           </li>
-          <li className="link-travelNav">
-            <a href="#none" className="link-box">
-              <i className="link-icon">🏖</i>
-              <span className="link-tit">travel</span>
+          <li>
+            <a href="#none">
+              <div className="card-cover">
+                <img src={img} alt="이미지" className="card-img" />
+              </div>
+              <div className="card-cont">
+                <div className="card-cate">
+                  <span className="card-cate-tit">Lorem, ipsum.</span>
+                  <span className="card-cate-star">⭐️⭐️⭐️⭐️⭐️</span>
+                </div>
+                <div className="card-desc">
+                  <div class="card-desc-tit multi-ellipsis"> Lorem ipsum dolor sit amet Lorem ipsum dolor
+                  sit amet consectetur adipisicing elit. Quibusdam, voluptatem?</div>
+                  <span class="card-desc-price">USD 9.45</span>
+                  <span class="card-desc-sale">10.22</span>
+                </div>
+              </div>
             </a>
           </li>
-          <li className="link-travelNav">
-            <a href="#none" className="link-box">
-              <i className="link-icon">🏖</i>
-              <span className="link-tit">travel</span>
+          <li>
+            <a href="#none">
+              <div className="card-cover">
+                <img src={img} alt="이미지" className="card-img" />
+              </div>
+              <div className="card-cont">
+                <div className="card-cate">
+                  <span className="card-cate-tit">Lorem, ipsum.</span>
+                  <span className="card-cate-star">⭐️⭐️⭐️⭐️⭐️</span>
+                </div>
+                <div className="card-desc">
+                  <div class="card-desc-tit multi-ellipsis"> Lorem ipsum dolor sit amet Lorem ipsum dolor
+                  sit amet consectetur adipisicing elit. Quibusdam, voluptatem?</div>
+                  <span class="card-desc-price">USD 9.45</span>
+                  <span class="card-desc-sale">10.22</span>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#none">
+              <div className="card-cover">
+                <img src={img} alt="이미지" className="card-img" />
+              </div>
+              <div className="card-cont">
+                <div className="card-cate">
+                  <span className="card-cate-tit">Lorem, ipsum.</span>
+                  <span className="card-cate-star">⭐️⭐️⭐️⭐️⭐️</span>
+                </div>
+                <div className="card-desc">
+                  <div class="card-desc-tit multi-ellipsis"> Lorem ipsum dolor sit amet Lorem ipsum dolor
+                  sit amet consectetur adipisicing elit. Quibusdam, voluptatem?</div>
+                  <span class="card-desc-price">USD 9.45</span>
+                  <span class="card-desc-sale">10.22</span>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href="#none">
+              <div className="card-cover">
+                <img src={img} alt="이미지" className="card-img" />
+              </div>
+              <div className="card-cont">
+                <div className="card-cate">
+                  <span className="card-cate-tit">Lorem, ipsum.</span>
+                  <span className="card-cate-star">⭐️⭐️⭐️⭐️⭐️</span>
+                </div>
+                <div className="card-desc">
+                  <div class="card-desc-tit multi-ellipsis"> Lorem ipsum dolor sit amet Lorem ipsum dolor
+                  sit amet consectetur adipisicing elit. Quibusdam, voluptatem?</div>
+                  <span class="card-desc-price">USD 9.45</span>
+                  <span class="card-desc-sale">10.22</span>
+                </div>
+              </div>
             </a>
           </li>
         </ul>
-      </nav>
+      </div>
       <nav>
         <div className="iconbox">
           <button>
