3a2bd0f4b1edfaae8f807dbdf251dcc5170f4895
jae <goawmfhfl1@naver.com>
2021-12-06 16:28:56 +0900

    feat: Mypage 레이아웃구성
    1. Mypage 레이아웃 페이지 구성

---
+++ src/components/pages/MYPAGE/MyProfile.js
@@ -0,0 +1,9 @@
+import './MyProfile.scss';
+
+function MyProfile() {
+    return (
+        <div></div>
+    )
+}
+
+export default MyProfile;
---
+++ src/components/pages/MYPAGE/MyTitle.js
@@ -0,0 +1,13 @@
+import './MyTitle.scss';
+
+function MyTitle() {
+    return (
+        <div>
+            <div className="mypage-container">
+                <h1 className="mypage-title">MyPage</h1>
+            </div>
+        </div>
+    )
+}
+
+export default MyTitle
