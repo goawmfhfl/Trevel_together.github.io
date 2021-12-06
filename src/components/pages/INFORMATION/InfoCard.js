28650cba12cd594cf238e09a93237b87c4d90ae1
jae <goawmfhfl1@naver.com>
2021-12-06 16:03:15 +0900

    feat: InfoPage Layout
    1. 인포메이션 레이아웃 구성완료

---
+++ src/components/pages/INFORMATION/InfoCard.js
@@ -0,0 +1,76 @@
+import './InfoCard.scss';
+import img from '../../../test.jpg';
+
+function InfoCard() {
+    return (
+        <div>
+            <ul className="infocard-list">
+                <li className="infocard-item">
+                    <img src={img} alt="" className="info-img" />
+                    <div className="info-content">
+                        <h3 className="info-content-tit sl-ellipsis">Lorem, ipsum Lorem, ipsum</h3>
+                        <span className="info-content-desc multi-ellipsis">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, consequatur! Lorem ipsum dolor sit amet.</span>
+                    </div>
+                </li>
+                <li className="infocard-item">
+                    <img src={img} alt="" className="info-img" />
+                    <div className="info-content">
+                        <h3 className="info-content-tit sl-ellipsis">Lorem, ipsum Lorem, ipsum</h3>
+                        <span className="info-content-desc multi-ellipsis">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, consequatur! Lorem ipsum dolor sit amet.</span>
+                    </div>
+                </li>
+                <li className="infocard-item">
+                    <img src={img} alt="" className="info-img" />
+                    <div className="info-content">
+                        <h3 className="info-content-tit sl-ellipsis">Lorem, ipsum Lorem, ipsum</h3>
+                        <span className="info-content-desc multi-ellipsis">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, consequatur! Lorem ipsum dolor sit amet.</span>
+                    </div>
+                </li>
+                <li className="infocard-item">
+                    <img src={img} alt="" className="info-img" />
+                    <div className="info-content">
+                        <h3 className="info-content-tit sl-ellipsis">Lorem, ipsum Lorem, ipsum</h3>
+                        <span className="info-content-desc multi-ellipsis">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, consequatur! Lorem ipsum dolor sit amet.</span>
+                    </div>
+                </li>
+                <li className="infocard-item">
+                    <img src={img} alt="" className="info-img" />
+                    <div className="info-content">
+                        <h3 className="info-content-tit sl-ellipsis">Lorem, ipsum Lorem, ipsum</h3>
+                        <span className="info-content-desc multi-ellipsis">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, consequatur! Lorem ipsum dolor sit amet.</span>
+                    </div>
+                </li>
+                <li className="infocard-item">
+                    <img src={img} alt="" className="info-img" />
+                    <div className="info-content">
+                        <h3 className="info-content-tit sl-ellipsis">Lorem, ipsum Lorem, ipsum</h3>
+                        <span className="info-content-desc multi-ellipsis">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, consequatur! Lorem ipsum dolor sit amet.</span>
+                    </div>
+                </li>
+                <li className="infocard-item">
+                    <img src={img} alt="" className="info-img" />
+                    <div className="info-content">
+                        <h3 className="info-content-tit sl-ellipsis">Lorem, ipsum Lorem, ipsum</h3>
+                        <span className="info-content-desc multi-ellipsis">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, consequatur! Lorem ipsum dolor sit amet.</span>
+                    </div>
+                </li>
+                <li className="infocard-item">
+                    <img src={img} alt="" className="info-img" />
+                    <div className="info-content">
+                        <h3 className="info-content-tit sl-ellipsis">Lorem, ipsum Lorem, ipsum</h3>
+                        <span className="info-content-desc multi-ellipsis">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, consequatur! Lorem ipsum dolor sit amet.</span>
+                    </div>
+                </li>
+                <li className="infocard-item">
+                    <img src={img} alt="" className="info-img" />
+                    <div className="info-content">
+                        <h3 className="info-content-tit sl-ellipsis">Lorem, ipsum Lorem, ipsum</h3>
+                        <span className="info-content-desc multi-ellipsis">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, consequatur! Lorem ipsum dolor sit amet.</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default InfoCard;
