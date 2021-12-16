e4520d5cc4112d26891fa1f06d7444359eea634f
jae <goawmfhfl1@naver.com>
2021-12-16 15:26:16 +0900

    feat: add styled-componenet
    1. card 스타일 컴포넌트 추가.
    2. travel 페이지와 product페이지에 적용

---
+++ src/components/Card.style.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Category = styled.span`
+  font-size: 1.2rem;
+`;
+export const Star = styled.span`
+  font-size: 1.2rem;
+`;
+export const Price = styled.span`
+  font-size: 1.2rem;
+`;
+export const Discount = styled.span`
+  font-size: 1.2rem;
+`;
