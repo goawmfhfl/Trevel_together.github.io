8363f2c0266ea69d949ad538867a851e9a182427
jae <goawmfhfl1@naver.com>
2021-12-16 16:18:13 +0900

    feat: ProductCont Add SC
    1. 프로덕트 Cont 페이지 스타일컴포넌트 추가
    2. 추후에 수정 필요

--- src/components/Card.style.js
+++ src/components/Card.style.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
+export const Info = styled.span`
+  font-size: 1.5rem;
+`;
+
 export const Category = styled.span`
   font-size: 1.2rem;
 `;
