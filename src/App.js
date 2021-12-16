9aa8e2948573b1755846b02004a96b3ea9192ff3
jae <goawmfhfl1@naver.com>
2021-12-16 11:46:06 +0900

    feat: add StyleComponent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-
+import styled from 'styled-components';
 import TravelPage from './components/pages/TRAVEL/Travel.js';
 import LikesPage from './components/pages/LIKES/Likes.js';
 import InformationPage from './components/pages/INFORMATION/Information.js';
@@ -10,45 +10,105 @@ import ProductsPage from './components/pages/PRODUCTS/products';
 
 import { Link, Route, Switch } from 'react-router-dom';
 
+const PageDiv = styled.div`
+  background-color: #fff;
+  max-width: 768px;
+`;
+const HeadContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+  font-weight: bold;
+  color: #000;
+`;
+const HeadTitle = styled.h1`
+  font-size: 3rem;
+`;
+const HeadIcon = styled.p`
+  font-size: 3rem;
+  margin-left: 0.5rem;
+`;
+const MainContainer = styled.div`
+  padding: 10px;
+`;
+
+const BotContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  padding: 24px;
+  color: #fff;
+  background-color: skyblue;
+  position: sticky;
+`;
+const NavIcon = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`;
+const Icon = styled.div`
+  font-size: 1.5rem;
+  margin-bottom: 0.3em;
+  color: #fff;
+`;
+const IconText = styled.div`
+  font-size: 1.2rem;
+  color: #fff;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="head-container">
-        <h1 className="head_title">Travel together</h1>
-        <p className="head_icon">✈️</p>
-      </header>
-      <div className="container">
-        <Route exact path="/" component={TravelPage} />
-        <Route exact path="/likes" component={LikesPage} />
-        <Route exact path="/information" component={InformationPage} />
-        <Route exact path="/mypage" component={MyPage} />
-        <Route exact path="/cart" component={CartPage} />
-        <Route exact path="/detail" component={ProductsPage} />
-      </div>
-      <nav>
-        <div className="iconbox">
-          <button>
-            <i className="fas fa-heart"></i>
-            <p>travel</p>
-          </button>
-          <button>
-            <i className="fas fa-heart"></i>
-            <p>travel</p>
-          </button>
-          <button>
-            <i className="fas fa-heart"></i>
-            <p>travel</p>
-          </button>
-          <button>
-            <i className="fas fa-heart"></i>
-            <p>travel</p>
-          </button>
-        </div>
-      </nav>
-    </div>
+    <>
+      <PageDiv calssName="App">
+        <header className="header">
+          <HeadContainer>
+            <HeadTitle>Travel together</HeadTitle>
+            <HeadIcon>✈️</HeadIcon>
+          </HeadContainer>
+        </header>
+        <hr />
+        <section className="section">
+          <MainContainer>
+            <Route exact path="/" component={TravelPage} />
+            <Route exact path="/likes" component={LikesPage} />
+            <Route exact path="/information" component={InformationPage} />
+            <Route exact path="/mypage" component={MyPage} />
+            <Route exact path="/cart" component={CartPage} />
+            <Route exact path="/detail" component={ProductsPage} />
+          </MainContainer>
+        </section>
+        <nav className="nav">
+          <BotContainer>
+            <a href="#none">
+              <NavIcon>
+                <Icon className="fas fa-heart"></Icon>
+                <IconText> icon1 </IconText>
+              </NavIcon>
+            </a>
+            <a href="#none">
+              <NavIcon>
+                <Icon className="fas fa-heart"></Icon>
+                <IconText> icon2 </IconText>
+              </NavIcon>
+            </a>
+            <a href="#none">
+              <NavIcon>
+                <Icon className="fas fa-heart"></Icon>
+                <IconText> icon3 </IconText>
+              </NavIcon>
+            </a>
+            <a href="#none">
+              <NavIcon>
+                <Icon className="fas fa-heart"></Icon>
+                <IconText> icon4 </IconText>
+              </NavIcon>
+            </a>
+          </BotContainer>
+        </nav>
+      </PageDiv>
+    </>
   );
 }
-
 
 export default App;
