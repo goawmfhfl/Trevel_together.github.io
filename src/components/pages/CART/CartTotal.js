c7fb5b5fc85a873376539a84fcd0eff4c07ff355
jae <goawmfhfl1@naver.com>
2021-12-17 15:38:27 +0900

    feat: Add StyleComponent
    1. 스타일컴포넌트화

--- src/components/pages/CART/CartTotal.js
+++ src/components/pages/CART/CartTotal.js
@@ -1,16 +1,49 @@
-import './CartTotal.scss'
-
+import styled from 'styled-components';
 
 function CardTotal() {
-    return (
-        <div className="cart-total-container">
-            <div className="cart-total">
-                <span className="cart-total-tit">TOTAL</span>
-                <strong className="cart-total-price">USD 46.65</strong>
-            </div>
-            <button className="cart-buybtn">Buy Now</button>
-        </div>
-    )
+  return (
+    <PayMentCont>
+      <TotalCont>
+        <Text>ToTal</Text>
+        <Price>USD 56.76</Price>
+      </TotalCont>
+      <BuyBtn>
+        <BtnText>Buy</BtnText>
+      </BuyBtn>
+    </PayMentCont>
+  );
 }
+const PayMentCont = styled.div`
+  margin-top: 1rem;
+  display: flex;
+  justify-content: space-around;
+  align-content: center;
+`;
+const TotalCont = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+const Text = styled.span`
+  font-size: 1rem;
+  text-align: center;
+  font-weight: bold;
+`;
+const Price = styled.span`
+  font-size: 1.2rem;
+  color: #2c88d9;
+  font-weight: bold;
+`;
+const BuyBtn = styled.button`
+  padding: 1rem 2rem;
+  border-radius: 10px;
+  background-color: #2c88d9;
+  color: #fff;
+  font-weight: bold;
+`;
+
+const BtnText = styled.span`
+  font-size: 1.2rem;
+`;
 
 export default CardTotal;
