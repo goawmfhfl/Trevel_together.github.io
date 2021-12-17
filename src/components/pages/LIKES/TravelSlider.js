a8fce3cbf65c679e4f6aa167290ddbca76b1ef9f
jae <goawmfhfl1@naver.com>
2021-12-17 11:30:31 +0900

    feat: Add Slider Style - Component
    1. Slider.js 파일 스타일컴포넌트 적용 완료

--- src/components/pages/LIKES/TravelSlider.js
+++ src/components/pages/LIKES/TravelSlider.js
@@ -1,4 +1,3 @@
-import './TravelSlider.scss';
 import testImg from '../../../test.jpg';
 import {
   Card,
@@ -17,128 +16,126 @@ import styled from 'styled-components';
 
 function TravelSlider() {
   return (
-    <div>
-      <div className="slider">
-        <div className="slider-title">
-          <h2 className="slider-tit">Travel</h2>
-          <div className="slider-icon">
-            <i>⬅</i>
-            <i>➡</i>
-          </div>
-        </div>
-        <Card>
-          <Link>
-            <LikesList>
-              <LikesItem>
-                <ImgCont>
-                  <Img src={testImg}></Img>
-                </ImgCont>
-                <DescCont>
-                  <CategoryCont>
-                    <Category>this is Category</Category>
-                    <Star>⭐️⭐️⭐️⭐️⭐️</Star>
-                  </CategoryCont>
-                  <Desc>
-                    Lorem ipsum dolor sit a m e t consectetur adipisicing
-                    veritatis sunt vel? Error velit expedita voluptate
-                  </Desc>
-                </DescCont>
-              </LikesItem>
-              <LikesItem>
-                <ImgCont>
-                  <Img src={testImg}></Img>
-                </ImgCont>
-                <DescCont>
-                  <CategoryCont>
-                    <Category>this is Category</Category>
-                    <Star>⭐️⭐️⭐️⭐️⭐️</Star>
-                  </CategoryCont>
-                  <Desc>
-                    Lorem ipsum dolor sit a m e t consectetur adipisicing
-                    veritatis sunt vel? Error velit expedita voluptate
-                  </Desc>
-                </DescCont>
-              </LikesItem>
-              <LikesItem>
-                <ImgCont>
-                  <Img src={testImg}></Img>
-                </ImgCont>
-                <DescCont>
-                  <CategoryCont>
-                    <Category>this is Category</Category>
-                    <Star>⭐️⭐️⭐️⭐️⭐️</Star>
-                  </CategoryCont>
-                  <Desc>
-                    Lorem ipsum dolor sit a m e t consectetur adipisicing
-                    veritatis sunt vel? Error velit expedita voluptate
-                  </Desc>
-                </DescCont>
-              </LikesItem>
-              <LikesItem>
-                <ImgCont>
-                  <Img src={testImg}></Img>
-                </ImgCont>
-                <DescCont>
-                  <CategoryCont>
-                    <Category>this is Category</Category>
-                    <Star>⭐️⭐️⭐️⭐️⭐️</Star>
-                  </CategoryCont>
-                  <Desc>
-                    Lorem ipsum dolor sit a m e t consectetur adipisicing
-                    veritatis sunt vel? Error velit expedita voluptate
-                  </Desc>
-                </DescCont>
-              </LikesItem>
-              <LikesItem>
-                <ImgCont>
-                  <Img src={testImg}></Img>
-                </ImgCont>
-                <DescCont>
-                  <CategoryCont>
-                    <Category>this is Category</Category>
-                    <Star>⭐️⭐️⭐️⭐️⭐️</Star>
-                  </CategoryCont>
-                  <Desc>
-                    Lorem ipsum dolor sit a m e t consectetur adipisicing
-                    veritatis sunt vel? Error velit expedita voluptate
-                  </Desc>
-                </DescCont>
-              </LikesItem>
-              <LikesItem>
-                <ImgCont>
-                  <Img src={testImg}></Img>
-                </ImgCont>
-                <DescCont>
-                  <CategoryCont>
-                    <Category>this is Category</Category>
-                    <Star>⭐️⭐️⭐️⭐️⭐️</Star>
-                  </CategoryCont>
-                  <Desc>
-                    Lorem ipsum dolor sit a m e t consectetur adipisicing
-                    veritatis sunt vel? Error velit expedita voluptate
-                  </Desc>
-                </DescCont>
-              </LikesItem>
-              <LikesItem>
-                <ImgCont>
-                  <Img src={testImg}></Img>
-                </ImgCont>
-                <DescCont>
-                  <CategoryCont>
-                    <Category>this is Category</Category>
-                    <Star>⭐️⭐️⭐️⭐️⭐️</Star>
-                  </CategoryCont>
-                  <Desc>
-                    Lorem ipsum dolor sit a m e t consectetur adipisicing
-                    veritatis sunt vel? Error velit expedita voluptate
-                  </Desc>
-                </DescCont>
-              </LikesItem>
-            </LikesList>
-          </Link>
-        </Card>
-      </div>
-    </div>
+    <Slider>
+      <Handler>
+        <Title>Title</Title>
+        <IconBox>
+          <Left>⬅</Left>
+          <Right>➡</Right>
+        </IconBox>
+      </Handler>
+      <Card>
+        <Link>
+          <LikesList>
+            <LikesItem>
+              <ImgCont>
+                <Img src={testImg}></Img>
+              </ImgCont>
+              <DescCont>
+                <CategoryCont>
+                  <Category>this is Category</Category>
+                  <Star>⭐️⭐️⭐️⭐️⭐️</Star>
+                </CategoryCont>
+                <Desc>
+                  Lorem ipsum dolor sit a m e t consectetur adipisicing
+                  veritatis sunt vel? Error velit expedita voluptate
+                </Desc>
+              </DescCont>
+            </LikesItem>
+            <LikesItem>
+              <ImgCont>
+                <Img src={testImg}></Img>
+              </ImgCont>
+              <DescCont>
+                <CategoryCont>
+                  <Category>this is Category</Category>
+                  <Star>⭐️⭐️⭐️⭐️⭐️</Star>
+                </CategoryCont>
+                <Desc>
+                  Lorem ipsum dolor sit a m e t consectetur adipisicing
+                  veritatis sunt vel? Error velit expedita voluptate
+                </Desc>
+              </DescCont>
+            </LikesItem>
+            <LikesItem>
+              <ImgCont>
+                <Img src={testImg}></Img>
+              </ImgCont>
+              <DescCont>
+                <CategoryCont>
+                  <Category>this is Category</Category>
+                  <Star>⭐️⭐️⭐️⭐️⭐️</Star>
+                </CategoryCont>
+                <Desc>
+                  Lorem ipsum dolor sit a m e t consectetur adipisicing
+                  veritatis sunt vel? Error velit expedita voluptate
+                </Desc>
+              </DescCont>
+            </LikesItem>
+            <LikesItem>
+              <ImgCont>
+                <Img src={testImg}></Img>
+              </ImgCont>
+              <DescCont>
+                <CategoryCont>
+                  <Category>this is Category</Category>
+                  <Star>⭐️⭐️⭐️⭐️⭐️</Star>
+                </CategoryCont>
+                <Desc>
+                  Lorem ipsum dolor sit a m e t consectetur adipisicing
+                  veritatis sunt vel? Error velit expedita voluptate
+                </Desc>
+              </DescCont>
+            </LikesItem>
+            <LikesItem>
+              <ImgCont>
+                <Img src={testImg}></Img>
+              </ImgCont>
+              <DescCont>
+                <CategoryCont>
+                  <Category>this is Category</Category>
+                  <Star>⭐️⭐️⭐️⭐️⭐️</Star>
+                </CategoryCont>
+                <Desc>
+                  Lorem ipsum dolor sit a m e t consectetur adipisicing
+                  veritatis sunt vel? Error velit expedita voluptate
+                </Desc>
+              </DescCont>
+            </LikesItem>
+            <LikesItem>
+              <ImgCont>
+                <Img src={testImg}></Img>
+              </ImgCont>
+              <DescCont>
+                <CategoryCont>
+                  <Category>this is Category</Category>
+                  <Star>⭐️⭐️⭐️⭐️⭐️</Star>
+                </CategoryCont>
+                <Desc>
+                  Lorem ipsum dolor sit a m e t consectetur adipisicing
+                  veritatis sunt vel? Error velit expedita voluptate
+                </Desc>
+              </DescCont>
+            </LikesItem>
+            <LikesItem>
+              <ImgCont>
+                <Img src={testImg}></Img>
+              </ImgCont>
+              <DescCont>
+                <CategoryCont>
+                  <Category>this is Category</Category>
+                  <Star>⭐️⭐️⭐️⭐️⭐️</Star>
+                </CategoryCont>
+                <Desc>
+                  Lorem ipsum dolor sit a m e t consectetur adipisicing
+                  veritatis sunt vel? Error velit expedita voluptate
+                </Desc>
+              </DescCont>
+            </LikesItem>
+          </LikesList>
+        </Link>
+      </Card>
+    </Slider>
   );
 }
 const LikesList = styled(CardList)`
@@ -150,6 +147,26 @@ const LikesItem = styled(CardItem)`
     margin-top: 0rem;
     margin-left: 2rem;
   }
+`;
+
+const Slider = styled.div`
+  margin-top: 1rem;
+  padding: 0.2rem;
+`;
+
+const Handler = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Title = styled.h2`
+  font-size: 1.2rem;
+`;
+const IconBox = styled.div``;
+const Left = styled.i`
+  font-size: 1.2rem;
+`;
+const Right = styled.i`
+  font-size: 1.2rem;
 `;
 
 export default TravelSlider;
